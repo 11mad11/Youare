24cf22cb1b7e59b16aef556e4e25c9424dcf6471
Marc-Antoine Desrochers <marcantoine@mad-pc.local>
2016-10-06 21:50:24 -0400

    some test on desktop for movement and starting of damage system

--- core/src/fr/mad/youare/item/ItemComponent.java
+++
@@ -1,8 +0,0 @@
-package fr.mad.youare.item;
-
-import com.badlogic.ashley.core.Component;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-
-public class ItemComponent implements Component{
-	
-}
--- core/src/fr/mad/youare/item/StateComponent.java
+++
@@ -1,9 +0,0 @@
-package fr.mad.youare.item;
-
-import com.badlogic.ashley.core.Component;
-
-public class StateComponent implements Component{
-
-	public String state;
-	
-}
